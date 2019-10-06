b65dcfa2cc3b261ed43d10d8504fd0fb0c1d1f11
Balázs Szabó <balazs.szabo@gmail.com>
2019-10-06 11:45:29 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { MatDialogRef, MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  isDarkTheme = false;
+
+  constructor() {
+  }
 }
